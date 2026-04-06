f9f9d55f888aaacd29bdc0afdb982d3fcaa485d9
davoriols <d4c7ooaz@anonaddy.me>
2026-04-06 18:01:52 +0200

    added analytics

---
+++ src/routes/+layout.ts
@@ -0,0 +1,5 @@
+// add analytics
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
